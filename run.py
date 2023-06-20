b8e948c8809172f85a5b3673c204fa7725d5e630
luohao-jk <luohao-jk@360shuke.com>
2023-06-20 22:47:56 +0800

    add threads

--- run.py
+++ run.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output_file', type=str, help='Output video file')
     parser.add_argument('-f', '--source_imgs', nargs='+', type=str, help='Image file paths')
     parser.add_argument('-n', '--max_frames', nargs='+', type=str, help='max frames to reface')
-    parser.add_argument('-tn', '--threads', nargs='+', type=int, help='threads', default=16)
+    parser.add_argument('-tn', '--threads', type=int, help='threads', default=16)
     args = parser.parse_args()
     main(args)
