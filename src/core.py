58a7d6e2b73f559588d90a2a41f18cb39508452f
luohao-jk <luohao-jk@360shuke.com>
2023-06-20 22:43:26 +0800

    add threads

--- src/core.py
+++ src/core.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import concurrent.futures
+import queue
 
 from moviepy.editor import VideoFileClip
 from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
@@ -17,14 +19,22 @@ def process_video(process_args):
     clip = VideoFileClip(process_args.input_file)
     progress = tqdm(total=int(clip.fps * clip.duration))
     processed_frames = []
-    for frame in clip.iter_frames():
-        processed_frame = handle_frame(frame, progress, process_args)
-        processed_frames.append(processed_frame)
-        if process_args.max_frames and len(processed_frames) >= process_args.max_frames:
-            break
+
+    # 创建有序队列
+    processed_queue = queue.Queue()
+
+    # 创建线程池
+    with concurrent.futures.ThreadPoolExecutor(max_workers=process_args.threads) as executor:
+        frame_infos = [(frame, progress, process_args) for frame in clip.iter_frames()]
+        # 使用多线程处理帧，并将处理后的帧放入队列
+        executor.map(processed_queue.put, executor.map(handle_frame, frame_infos))
+
+    # 从队列中按顺序取出处理后的帧
+    while not processed_queue.empty():
+        processed_frames.append(processed_queue.get())
 
     processed_clip = ImageSequenceClip(processed_frames, durations=[1/clip.fps] * len(processed_frames), fps=clip.fps)
-    processed_clip.write_videofile(process_args.output_file)
+    processed_clip.write_videofile(process_args.output_file, threads=process_args.threads)
     progress.close()
 
 
