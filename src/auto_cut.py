325906ba91532c35a59e8a21a32099f7f66d1e40
luohao-jk <luohao-jk@360shuke.com>
2023-06-23 14:57:41 +0800

    并发extract_frames

--- src/auto_cut.py
+++ src/auto_cut.py
@@ -1,6 +1,7 @@
 import concurrent.futures
 import json
 import os
+import threading
 
 from moviepy.video.compositing.concatenate import concatenate_videoclips
 from moviepy.video.io.VideoFileClip import VideoFileClip
@@ -35,8 +36,8 @@ def get_index_range(arr, accept_min_size):
     return accept_range
 
 
-def is_accept(frame, index, accept_infos, progress, args):
-    many_faces = get_face_many(frame)
+def is_accept(clip, t, index, accept_infos, progress, args):
+    many_faces = get_face_many(clip.get_frame(t))
     male_faces = [face for face in many_faces if face['gender'] == 1]
     female_faces = [face for face in many_faces if face['gender'] == 0]
     accept = args.female_min <= len(female_faces) <= args.female_max and args.male_min <= len(
@@ -45,54 +46,79 @@ def is_accept(frame, index, accept_infos, progress, args):
     progress.update(1)
 
 
+def copy_and_read_video(args, clips):
+    with open(args.input_file, 'rb') as file:
+        data = file.read()
+        print(f"拷贝数据，读取成功，长度：{len(data)}")
+    for i in range(args.copies):
+        with open(f"cache/{args.input_file}_bak.{i}", 'wb') as copy_file:
+            copy_file.write(data)
+            copy_file.flush()
+            clips[i + 1] = VideoFileClip(f"cache/{args.input_file}_bak.{i}")
+            print(f"拷贝数据，写入成功：{args.input_file}_bak.{i}")
+
+
 def cut_video(args):
-    clip = VideoFileClip(args.input_file)
-    if args.gap_time < 1.0 / clip.fps:
-        args.gap_time = 1.0 / clip.fps
+    clips = [None] * (args.copies + 1)
+    threading.Thread(target=copy_and_read_video, args=(args, clips))
+    clips[0] = VideoFileClip(args.input_file)
+    fps = clips[0].fps
+    if args.gap_time < 1.0 / fps:
+        args.gap_time = 1.0 / fps
         print(f"gap time 过低，重置为1/fps={args.gap_time}")
-    accept_infos = [False] * int(clip.duration / args.gap_time)
+    accept_infos = [False] * int(clips[0].duration / args.gap_time)
     get_face_analyser()
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
         index = 0
         t = 0
         min_time = max(0, args.min_time)
-        max_time = min(clip.duration, args.max_time)
+        max_time = min(clips[0].duration, args.max_time)
         progress = tqdm(total=int((max_time - min_time) / args.gap_time))
-        while t <= clip.duration and index < len(accept_infos):
+        clip_index = 0
+        while t <= clips[0].duration and index < len(accept_infos):
             if args.max_time >= t >= args.min_time:
-                frame = clip.get_frame(t)
-                executor.submit(is_accept, frame, index, accept_infos, progress, args)
+                if (not clips[clip_index]) or clip_index >= len(clips):
+                    clip_index = 0
+                print(f"提交第{index}帧处理任务，clip_index={clip_index}")
+                executor.submit(is_accept, clips[clip_index], t, index, accept_infos, progress, args)
+                clip_index += 1
             if t >= args.max_time:
                 break
             t += args.gap_time
             index += 1
 
     cut_times = get_index_range(accept_infos, args.accept_min_time * 1.0 / args.gap_time)
-    sub_clips = []
-    tmp_clip = VideoFileClip(args.input_file)
-    sum_time = 0
+    for cut_time in cut_times:
+        for i in range(len(cut_time)):
+            cut_time[i] *= args.gap_time
+
     with open(f"{args.input_file}.txt", 'w') as file:
         cut_info = {
             "cut_times": cut_times,
             "gap_time": args.gap_time
         }
         file.write(json.dumps(cut_info))
-    print(cut_times)
+
+    do_cut_video(args, cut_times)
+
+
+def do_cut_video(args, cut_times):
+    sub_clips = []
+    tmp_clip = VideoFileClip(args.input_file)
+    sum_time = 0
     for cut_time in cut_times:
         s, e = cut_time
-        s = s * args.gap_time
-        e = e * args.gap_time
         print(f"实际使用的时间范围[{s}, {e}]")
         sum_time += e - s
         try:
             sc = tmp_clip.subclip(s, e)
-            sc = sc.set_audio(tmp_clip.audio.subclip(s,e))
+            sc = sc.set_audio(tmp_clip.audio.subclip(s, e))
             sub_clips.append(sc)
         except:
             print(f"提取片段时出现异常，片段:[{s}, {e}],")
             continue
 
-    print(f"原时间:{clip.duration}，剪辑后的时长:{sum_time}")
+    print(f"原时间:{tmp_clip.duration}，剪辑后的时长:{sum_time}")
     final_clip = concatenate_videoclips(sub_clips)
     final_clip.write_videofile(args.output_file, threads=args.threads)
