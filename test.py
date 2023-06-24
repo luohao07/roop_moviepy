d115fb584b9139a605fc4e4610665b06d8b64f55
luohao-jk <luohao-jk@360shuke.com>
2023-06-24 16:16:42 +0800

    test audio

--- test.py
+++ test.py
@@ -5,12 +5,9 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 
 def cut_with_audio(input_file, output_file):
     clip = VideoFileClip(input_file)
-    audio = clip.audio
     video_sub_clip = clip.subclip(100, 150)
-    audio_sub_clip = audio.subclip(100, 150)
-    video_sub_clip = video_sub_clip.set_audio(audio_sub_clip)
+    video_sub_clip = video_sub_clip.set_audio(clip.audio.subclip(100, 150))
     video_sub_clip.write_videofile(output_file, codec='libx264')
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Video processing using MoviePy')
