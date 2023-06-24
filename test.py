85c11f39402d645e7e4a3fcd1d49fe935ad708a5
luohao-jk <luohao-jk@360shuke.com>
2023-06-24 16:14:20 +0800

    test audio

--- test.py
+++ test.py
@@ -7,8 +7,9 @@ def cut_with_audio(input_file, output_file):
     clip = VideoFileClip(input_file)
     audio = clip.audio
     video_sub_clip = clip.subclip(100, 150)
-    audio.write_audiofile(output_file)
-    # video_sub_clip.write_videofile(output_file, codec='libx264')
+    audio_sub_clip = audio.subclip(100, 150)
+    video_sub_clip = video_sub_clip.set_audio(audio_sub_clip)
+    video_sub_clip.write_videofile(output_file, codec='libx264')
 
 
 if __name__ == '__main__':
