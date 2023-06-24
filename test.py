8189fd1b2f40cbf48291fb698f7fcae55b9c317e
luohao-jk <luohao-jk@360shuke.com>
2023-06-24 15:34:08 +0800

    test audio

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 import argparse
 
-from test.audio_test import cut_with_audio
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+
+def cut_with_audio(args):
+    clip = VideoFileClip(args.input_file)
+    video_sub_clip = clip.subclip(100, 150)
+    audio_sub_clip = clip.audio.subclip(100, 150)
+
+    video_sub_clip.set_audio(audio_sub_clip)
+
+    video_sub_clip.write_videofile(args.output_file)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Video processing using MoviePy')
