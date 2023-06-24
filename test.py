748eb4559258fe9380f9de7fe06b5d9f8911fa2e
luohao-jk <luohao-jk@360shuke.com>
2023-06-24 16:08:13 +0800

    test audio

--- test.py
+++ test.py
@@ -3,14 +3,11 @@ import argparse
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
 
-def cut_with_audio(args):
-    clip = VideoFileClip(args.input_file)
+def cut_with_audio(input, output):
+    clip = VideoFileClip(input)
     video_sub_clip = clip.subclip(100, 150)
-    audio_sub_clip = clip.audio.subclip(100, 150)
-
-    video_sub_clip.set_audio(audio_sub_clip)
     print(args.output_file)
-    video_sub_clip.write_videofile(args.output_file, codec='libx264')
+    video_sub_clip.write_videofile(output, codec='libx264')
 
 
 if __name__ == '__main__':
@@ -18,4 +15,4 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--input_file', type=str, help='Input video file')
     parser.add_argument('-o', '--output_file', type=str, help='Output video file')
     args = parser.parse_args()
-    cut_with_audio(args)
+    cut_with_audio(args.input_file, args.output_file)
