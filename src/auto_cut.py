e47b89b2afc19dd8534e07d882e05eb7e7644083
luohao-jk <luohao-jk@360shuke.com>
2023-06-24 17:01:53 +0800

    多文件处理

--- src/auto_cut.py
+++ src/auto_cut.py
@@ -1,5 +1,7 @@
 import concurrent.futures
 import json
+import os
+import shutil
 import time
 
 from moviepy.video.compositing.concatenate import concatenate_videoclips
@@ -47,27 +49,49 @@ def is_accept(frame, index, accept_infos, progress, args):
     progress.update(1)
 
 
-def cut_video_wrap(args):
-    start_time = time.perf_counter()
-    clip = VideoFileClip(args.input_file)
+def copy_input_file(args):
+    basename = os.path.basename(args.input_file)
+    name, extension = os.path.splitext(basename)
+    files = [args.input_file]
+    for index in range(args.copies):
+        copy_file_path = f"{os.path.dirname(args.input_file)}/{name}_copy{index}{extension}"
+        shutil.copy(args.input_file, copy_file_path)
+        print(f"复制第{index}个文件成功：{copy_file_path}")
+        files.append(copy_file_path)
+    return files
 
-    accept_infos = [None] * int(clip.duration * clip.fps)
+
+def cut_video_wrap(args):
+    files = copy_input_file(args)
+    start_time = time.perf_counter()
+    clips = []
+    for file in files:
+        clips.append(VideoFileClip(file))
+    accept_infos = [None] * int(clips[0].duration * clips[0].fps)
 
     for index, gap_time in enumerate(args.gap_times):
+        progress = tqdm(total=clips[0].duration / args.gap_time)
         print(f"开始第index轮剪辑gap_time={gap_time}，当前待检测帧{accept_infos.count(None)}，",
               f"已过滤帧{accept_infos.count(False)}, 已接受帧{accept_infos.count(True)}")
         args.gap_time = gap_time
-        cut_video(clip, accept_infos, args)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=len(clips)) as executor:
+            for task_i in range(args.copies + 1):
+                clip = VideoFileClip(files[task_i])
+                start_time = clips[0].duration / len(clips) * task_i
+                end_time = clips[0].duration / len(clips) * (task_i + 1)
+                print(f"提交第{index}轮第{task_i + 1}个任务，start_time={start_time}, end_time = {end_time}")
+                executor.submit(cut_video, args=(clip, accept_infos, args, start_time, end_time, progress))
+        # 如果任意两个时间差小于accept_min_time的帧都为False，那中间的部分就不用检测了，直接设置为False
+        set_false_between(accept_infos, args.accept_min_time * clip.fps)
 
     cut_frames = get_index_range(accept_infos, args.accept_min_time * clip.fps)
     for arr in cut_frames:
         for i in range(len(arr)):
             arr[i] = arr[i] * 1.0 / clip.fps
-
     cut_times = cut_frames
 
     try:
-        new_clip = do_cut_to_clip(clip, args, cut_times)
+        new_clip = do_cut_to_clip(clips[0], args, cut_times)
         new_clip.write_videofile(args.output_file, threads=args.threads, audio_codec='aac')
     except:
         print(f"合成失败！文件占用，现场已保存，可使用以下命令重试合成操作: ",
@@ -88,7 +112,7 @@ def set_false_between(array, min_size):
     return array
 
 
-def cut_video(clip, accept_infos, args):
+def cut_video(clip, accept_infos, args, start_time, end_time, progress):
     if args.gap_time < 1.0 / clip.fps:
         args.gap_time = 1.0 / clip.fps
         print(f"gap time 过低，重置为1/fps={args.gap_time}")
@@ -96,10 +120,9 @@ def cut_video(clip, accept_infos, args):
 
     fail_count = 0
     with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
-        index = 0
-        t = 0
-        progress = tqdm(total=clip.duration / args.gap_time)
-        while t <= clip.duration and index < len(accept_infos):
+        t = start_time
+        index = int(t * clip.fps)
+        while t <= end_time and index < len(accept_infos):
             if accept_infos[index] is None:
                 try:
                     frame = clip.get_frame(t)
@@ -115,9 +138,6 @@ def cut_video(clip, accept_infos, args):
             t += args.gap_time
             index = int(t * clip.fps)
         print(f"任务提交结束：{t}, {index}, {len(accept_infos)}")
-
-    # 如果任意两个时间差小于accept_min_time的帧都为False，那中间的部分就不用检测了，直接设置为False
-    set_false_between(accept_infos, args.accept_min_time * clip.fps)
 
 
 def do_cut_to_clip(clip, args, cut_times, save_log = True):
--- test/audio_extract.py
+++ test/audio_extract.py
@@ -1,4 +1,5 @@
 import os
+
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
 
