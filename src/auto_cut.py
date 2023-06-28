fc30b3d0518849099c0f58f85c4276c6507219b5
luohao-jk <luohao-jk@360shuke.com>
2023-06-28 22:08:36 +0800

    gt fix

--- src/auto_cut.py
+++ src/auto_cut.py
@@ -121,8 +121,7 @@ def cut_video_wrap(args):
         if gap_time < 1.0 / clips[0].fps:
             gap_time = 1.0 / clips[0].fps
             print(f"gap time 过低，重置为1/fps={gap_time}")
-        progress = tqdm((args.max_time - args.min_time) * clips[0].fps / gap_time)
-        print(args)
+        progress = tqdm(int((args.max_time - args.min_time) * clips[0].fps / gap_time))
         print(f"开始第{index}轮剪辑gap_time={gap_time}，当前待检测帧{accept_infos.count(None)}，",
               f"已过滤帧{accept_infos.count(False)}, 已接受帧{accept_infos.count(True)}")
         args.gap_time = gap_time
